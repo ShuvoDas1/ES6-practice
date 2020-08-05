1bbdd7a53a14ca7ca40717f5022335b4aade4acc
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-06 02:22:46 +0600

    template

---
+++ template.js
@@ -0,0 +1,12 @@
+// template
+const firstName = "Shuvo";
+const lastName = "das";
+// const fullName = firstName + " " + lastName + " is a good boy";//old 
+const fullName2 =  `${firstName} ${lastName}`;// template string
+console.log(fullName2);
+
+const multipleLine = `hi
+I am shuvo
+do you hear me
+i know you forget me.`;
+console.log(multipleLine);//(``) use this  symbol in multiple line string
